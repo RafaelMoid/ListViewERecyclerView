48c1fc02149347fd7ef875807192ba73ce1c6e21
Rafael Varela <rafaelvrcr@gmail.com>
2021-06-03 14:02:53 -0300

    List view criado, realizando testes.

---
+++ app/src/main/java/com/example/listviewerecyclerview/ListViewActivity.java
@@ -0,0 +1,35 @@
+package com.example.listviewerecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec"
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listLocais = findViewById(R.id.listLocais);
+
+        //Criar adaptador para a lista
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        //Adicionar adaptador a lista
+        listLocais.setAdapter( adaptador );
+    }
+}
