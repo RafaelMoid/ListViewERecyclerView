4fbe0cf072e8ffb1611fde372179d6acb3b20d3e
Rafael Varela <rafaelvrcr@gmail.com>
2021-06-03 14:40:14 -0300

    List view criado e testado validado com Toast on iten click

--- app/src/main/java/com/example/listviewerecyclerview/ListViewActivity.java
+++ app/src/main/java/com/example/listviewerecyclerview/ListViewActivity.java
@@ -3,13 +3,20 @@ package com.example.listviewerecyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListViewActivity extends AppCompatActivity {
 
     private ListView listLocais;
     private String[] itens = {
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec",
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec",
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec",
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec",
             "New brunwswick", "halifax", "labrador & newfoundland", "Quebec"
     };
 
@@ -31,5 +38,15 @@ public class ListViewActivity extends AppCompatActivity {
 
         //Adicionar adaptador a lista
         listLocais.setAdapter( adaptador );
+
+        //Adicionar click listener
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String valorSeleciondo = listLocais.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), valorSeleciondo, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/listviewerecyclerview/MainActivity.java
+++ app/src/main/java/com/example/listviewerecyclerview/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.listviewerecyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec",
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec",
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec",
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec",
+            "New brunwswick", "halifax", "labrador & newfoundland", "Quebec"
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listLocais = findViewById(R.id.listLocais);
+
+        //Criar adaptador para a lista
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        //Adicionar adaptador a lista
+        listLocais.setAdapter( adaptador );
+
+        //Adicionar click na lista
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String valorSelecionado = listLocais.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), valorSelecionado, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
